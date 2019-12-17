b9593ccee9467cd95cdb54a2c247f8e32f1cfd87
brandiw <brandimichelle.0911@gmail.com>
2019-12-17 11:52:40 -0800

    Adding hook and validations

--- models/user.js
+++ models/user.js
@@ -1,18 +1,64 @@
-'use strict';
+'use strict'
+
+let bcrypt = require('bcryptjs')
+
 module.exports = (sequelize, DataTypes) => {
   const user = sequelize.define('user', {
-    firstname: DataTypes.STRING,
+    firstname: {
+      type: DataTypes.STRING,
+      validate: {
+        notNull: {
+          msg: 'Come on, we know you have a firstname.'
+        }
+      }
+    },
     lastname: DataTypes.STRING,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          msg: 'Hey, please give a valid email address! D:'
+        }
+      }
+    },
     username: DataTypes.STRING,
     birthdate: DataTypes.DATE,
-    password: DataTypes.STRING,
-    photoUrl: DataTypes.STRING,
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [6, 25],
+          msg: 'Your password must be between 6 and 25 characters. Try again!'
+        }
+      }
+    },
+    photoUrl: {
+      type: DataTypes.STRING,
+      validate: {
+        isUrl: {
+          msg: 'Aww sad, no picture? :('
+        }
+      }
+    },
     bio: DataTypes.TEXT,
     admin: DataTypes.BOOLEAN
-  }, {});
+  }, {
+    hooks: {
+      beforeCreate: pendingUser => {
+        if (pendingUser && pendingUser.password) {
+          // Hash the password
+          let hashedPassword = bcrypt.hashSync(pendingUser.password, 12)
+
+          // Reassign the password field to hashed value
+          pendingUser.password = hashedPassword
+        }
+      }
+    }
+  })
+
   user.associate = function(models) {
     // associations can be defined here
-  };
-  return user;
-};
+  }
+
+  return user
+}
