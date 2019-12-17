60964f9060b3abe38f174266b764b6146f1d4ffa
brandiw <brandimichelle.0911@gmail.com>
2019-12-17 11:59:44 -0800

    Added helper function validatePassword to user model

--- models/user.js
+++ models/user.js
@@ -60,5 +60,13 @@ module.exports = (sequelize, DataTypes) => {
     // associations can be defined here
   }
 
+  user.prototype.validPassword = function(typedInPassword) {
+    // Determine if typed-in password hashes to same thing as existing hash
+    let correctPassword = bcrypt.compareSync(typedInPassword, this.password)
+
+    // Return the result of that comparison
+    return correctPassword
+  }
+
   return user
 }
